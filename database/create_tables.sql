16660d61daa4b3e6f25975c61c67df830ac4cf28
Henry Tirado <85540489+githotirado@users.noreply.github.com>
2021-10-14 07:28:35 -0700

    Fixed db import. Imported all rows to AWS Postgres

--- database/create_tables.sql
+++ database/create_tables.sql
@@ -1,6 +1,6 @@
-drop table if exists vehicleCount2020;
+drop table if exists vehiclecount2020;
 
-create table vehicleCount2020 (
+create table vehiclecount2020 (
 	date DATE,
 	zip_code VARCHAR,
 	model_year VARCHAR,
@@ -10,12 +10,4 @@ create table vehicleCount2020 (
 	vehicles INT
 );
 
-select * from vehicleCount2020;
-
-insert into vehicleCount2020
-values ('2020-01-01', '90027', '2018', 'Hybrid', 'Nissan', 'light', 35);
-
--- COPY vehicleCount2020(date, zip_code, model_year, fuel, make, duty, vehicles)
--- FROM '/Users/henrytirado/git/usc_homeworkb/tbdProject3/template/static/data/vehicle-count-as-of-1-1-2020.csv'
--- DELIMITER ','
--- CSV HEADER;
+select count(*) from vehiclecount2020;
